58f220d3d4516b517ed86dd88b579d96612b73e3
wouter.lenaerts <wouter.lenaerts1@student.kuleuven.be>
2021-03-27 15:56:59 +0100

    4 behaviour classes

---
+++ src/main/java/agent/behaviour/dropPacket/subBehaviours/MoveToDestination.java
@@ -0,0 +1,61 @@
+package agent.behaviour.dropPacket.subBehaviours;
+
+import agent.AgentImp;
+import agent.behaviour.LTDBehaviour;
+import environment.Coordinate;
+import agent.behaviour.dropPacket.DropPacket;
+import environment.Perception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoveToDestination extends LTDBehaviour {
+    @Override
+    public void act(AgentImp agent) {
+        moveTo(agent, Coordinate.fromString(agent.getMemoryFragment(DropPacket.DESTINATION_KEY)));
+    }
+
+    @Override
+    public void communicate(AgentImp agent) {
+
+    }
+
+    private final static List<Coordinate> POSSIBLE_MOVES = new ArrayList<Coordinate>(List.of(
+            new Coordinate(1, 1), new Coordinate(-1, -1),
+            new Coordinate(1, 0), new Coordinate(-1, 0),
+            new Coordinate(0, 1), new Coordinate(0, -1),
+            new Coordinate(1, -1), new Coordinate(-1, 1)
+    ));
+
+    private void moveTo(AgentImp agent, Coordinate destination){
+        var perception = agent.getPerception();
+        Coordinate agentCoord = new Coordinate(agent.getX(), agent.getY());
+        Coordinate currentBestMove = null;
+        for (var move : POSSIBLE_MOVES) {
+            int x = move.getX();
+            int y = move.getY();
+            if (perception.getCellPerceptionOnRelPos(x, y) != null && perception.getCellPerceptionOnRelPos(x, y).isWalkable()) {
+                if (currentBestMove == null){
+                    currentBestMove = move;
+                }
+                else if (isCloser(Coordinate.getSum(agentCoord, move), Coordinate.getSum(agentCoord, currentBestMove), destination))
+                    currentBestMove = move;
+            }
+        }
+
+        if (currentBestMove == null) agent.skip(); //TODO: Zou dit mogen voorvallen???
+        else agent.step(agent.getX() + currentBestMove.getX(), agent.getY() + currentBestMove.getY());
+    }
+
+    private boolean isCloser(Coordinate first, Coordinate second, Coordinate dest){
+        if (first == null) {
+            return false;
+        }else if (second == null){
+            return true;
+        }else{
+            int distDestToFirst = Perception.distance(dest.getX(), dest.getY(), first.getX(), first.getY());
+            int distDestToSecond = Perception.distance(dest.getX(), dest.getY(), second.getX(), second.getY());
+            return distDestToFirst < distDestToSecond;
+        }
+    }
+}
--- src/main/java/agent/behaviour/dropPacket/subBehaviours/PickPacket.java
+++ src/main/java/agent/behaviour/dropPacket/subBehaviours/PickPacket.java
@@ -5,16 +5,16 @@ import agent.behaviour.LTDBehaviour;
 import environment.Coordinate;
 import agent.behaviour.dropPacket.DropPacket;
 
-public class PutPacket extends LTDBehaviour {
+public class PickPacket extends LTDBehaviour {
 
     @Override
     public void act(AgentImp agent) {
         Coordinate destination = Coordinate.fromString(agent.getMemoryFragment(DropPacket.DESTINATION_KEY));
         try {
-            agent.putPacket(destination.getX(), destination.getY());
+            agent.pickPacket(destination.getX(), destination.getY());
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
-            // If destination changed, destination will become null and agent should go to move randomly
+            // If packet is already gone, destination will become null and agent should go to move randomly
         }
         agent.removeMemoryFragment(DropPacket.DESTINATION_KEY);
         agent.addMemoryFragment(DropPacket.SEARCH_ALL_KEY, "true");
