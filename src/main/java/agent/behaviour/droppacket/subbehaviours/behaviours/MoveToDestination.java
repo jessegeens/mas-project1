61d92c3783f053f65c1a01cf3bced450bd1eeb22
Joris Ignoul <joris.ignoul193@gmail.com>
2021-04-11 14:09:04 +0200

    wip

--- src/main/java/agent/behaviour/droppacket/subbehaviours/behaviours/MoveToDestination.java
+++ src/main/java/agent/behaviour/droppacket/subbehaviours/behaviours/MoveToDestination.java
@@ -56,6 +56,57 @@ public class MoveToDestination extends LTDBehaviour {
         }
     }
 
+    private double calculateDegreeWithXAxis(Coordinate c1, Coordinate c2){
+        int deltaX = c1.getX() - c2.getX();
+        int deltaY = c1.getY() - c2.getY();
+        double rad = Math.atan2(deltaX,deltaY)+Math.PI/2;
+        if(rad<0)
+            rad+=2*Math.PI;
+        return rad;
+    }
+
+    //https://www.geeksforgeeks.org/program-for-point-of-intersection-of-two-lines/
+    private Coordinate calcIntersectionOfLines(Coordinate A, Coordinate B, Coordinate C, Coordinate D){
+        // Line AB represented as a1x + b1y = c1
+        double a1 = B.getY() - A.getY();
+        double b1 = A.getX() - B.getX();
+        double c1 = a1*(A.getX()) + b1*(A.getY());
+
+        // Line CD represented as a2x + b2y = c2
+        double a2 = D.getY() - C.getY();
+        double b2 = C.getX() - D.getX();
+        double c2 = a2*(C.getX())+ b2*(C.getY());
+
+        double determinant = a1*b2 - a2*b1;
+
+        if (determinant == 0)
+        {
+            // The lines are parallel. This is simplified
+            // by returning a pair of FLT_MAX
+            throw new IllegalStateException("Shouldn't happen in our use case");
+        }
+        else
+        {
+            int x = (int) Math.floor((b2*c1 - b1*c2)/determinant);
+            int y = (int) Math.floor((a1*c2 - a2*c1)/determinant);
+            return new Coordinate(x, y);
+        }
+    }
+
+    private Coordinate calcIntersectionWithPerception(Coordinate agent, Coordinate destination){
+    double rad = calculateDegreeWithXAxis(agent,destination);
+    if(rad<1/4*Math.PI || rad > 7/4*Math.PI) {
+        //right boundary
+    }else if(rad<3/4*Math.PI){
+        //top boundary
+    }else if(rad<5/4*Math.PI){
+        //left boundary
+    }else {
+        //bottom boundary
+    }
+        return null;
+    }
+
     private List<Coordinate> calculateDijkstra(Coordinate destination, Perception perception) {
         Coordinate currentPos = new Coordinate(perception.getSelfX(), perception.getSelfY());
         PriorityQueue<ArrayList<Coordinate>> pq = new PriorityQueue<>(Comparator.comparingInt(ArrayList::size));
