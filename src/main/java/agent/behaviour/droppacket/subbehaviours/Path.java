689ea298eab17d57322e1011e283eaf9f6b0d496
wouter.lenaerts <wouter.lenaerts1@student.kuleuven.be>
2021-04-24 13:47:38 +0200

    path class

---
+++ src/main/java/agent/behaviour/droppacket/subbehaviours/Path.java
@@ -0,0 +1,46 @@
+package agent.behaviour.droppacket.subbehaviours;
+
+import agent.AgentImp;
+import environment.CellPerception;
+import environment.Coordinate;
+import environment.world.agent.Agent;
+
+import java.util.List;
+
+public class Path {
+
+    public Path() {
+    }
+
+    public Boolean isWalkablePath() {
+        return false;
+    }
+
+    public List<Coordinate> getPathCoordinate() {
+        return null;
+    }
+
+    public List<CellPerception> getPacketCoordinatesInPath() {
+        return null;
+    }
+
+    public CellPerception getEndDestination() {
+        return null;
+    }
+
+    public boolean isFinalDestination() {
+        return false;
+    }
+
+    public AgentImp getCaller() {
+        return null;
+    }
+
+    public String toString() {
+        return "";
+    }
+
+    public static Path stringToPath(String path) {
+        return null;
+    }
+}
